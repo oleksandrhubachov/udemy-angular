805edce695dce9665851821eeaa5f77814cc9cc7
Bishop-dev <alex.bishop@yandex.ru>
2020-05-01 09:11:13 +0300

    111 - injecting services into services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { UnlessDirective } from './unless/unless.directive';
 import { DropdownDirective } from './shared/dropdown.directive';
 import {AccountComponent} from './account/account.component';
 import {NewAccountComponent} from './new-account/new-account.component';
+import {AccountsService} from './accounts.service';
+import {LoggingService} from './logging.service';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,7 @@ import {NewAccountComponent} from './new-account/new-account.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,8 +5,8 @@ import {AccountsService} from '../accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -15,7 +15,7 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.onStatusChange(accountStatus);
+    // this.loggingService.onStatusChange(accountStatus);
   }
 
 }
