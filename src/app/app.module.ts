1192ef900cbb3384f9cc743b72a8c86a5f6fdaab
Bishop-dev <alex.bishop@yandex.ru>
2020-05-03 22:25:20 +0300

    125 - understanding the example project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless/unless.directive';
-import { DropdownDirective } from './shared/dropdown.directive';
-import {AccountComponent} from './account/account.component';
-import {NewAccountComponent} from './new-account/new-account.component';
-import {AccountsService} from './accounts.service';
-import {LoggingService} from './logging.service';
-import {ActiveUsersComponent} from './active-users/active-users.component';
-import {InactiveUsersComponent} from './inactive-users/inactive-users.component';
-import {UserService} from './user.service';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    AccountComponent,
-    NewAccountComponent,
-    BasicHighlightDirective,
-    BetterHighlightDirective,
-    UnlessDirective,
-    DropdownDirective,
-    ActiveUsersComponent,
-    InactiveUsersComponent
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [AccountsService, LoggingService, UserService, ShoppingListService],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
