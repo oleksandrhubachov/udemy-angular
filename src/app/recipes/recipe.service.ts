d77cbde2664dddb9347a7c76124c2b28efdb0d49
Bishop-dev <alex.bishop@yandex.ru>
2020-05-03 21:40:43 +0300

    121 - Adding ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,29 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
-    new Recipe('A Test Recipe2', 'This is simply a test2', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+    new Recipe(
+      'A Test Recipe1',
+      'This is simply a test1',
+      'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'A Test Recipe2',
+      'This is simply a test2',
+      'https://cdn11.bigcommerce.com/s-9eg277ar0n/images/stencil/original/y/banner-feed-me-family__38360.original.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
 
   getRecipes() {
