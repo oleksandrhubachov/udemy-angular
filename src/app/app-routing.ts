377e96f70dd5a9496456e0facd9040f41f324d32
Bishop-dev <alex.bishop@yandex.ru>
2020-05-05 18:09:37 +0300

    147 - protecting child (nested) routes with canActivateChild

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -14,7 +14,12 @@ const appRoutes: Routes = [
   {path: 'users', component: UsersComponent, children: [
       {path: ':id/:name', component: UserComponent}
     ]},
-  {path: 'servers', canActivate: [AuthGuardService], component: ServersComponent, children: [
+  {
+    path: 'servers',
+    // canActivate: [AuthGuardService],
+    canActivateChild: [AuthGuardService],
+    component: ServersComponent,
+    children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]},
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,10 +1,10 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth-service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
 
   constructor(private authService: AuthService,
               private router: Router) {
@@ -19,6 +19,10 @@ export class AuthGuardService implements CanActivate {
         // return false;
       }
     });
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivate(childRoute, state);
   }
 
 }
