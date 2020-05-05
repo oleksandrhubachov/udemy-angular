07f0a2304a4ba963fe27fb1891e1cfdfc9c91d60
Bishop-dev <alex.bishop@yandex.ru>
2020-05-05 21:01:27 +0300

    178 - useful resources and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {UserService} from './user.service';
-import {Subscription} from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated: boolean = false;
-  private activatedSub: Subscription;
-  constructor(private userService: UserService) {}
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    });
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
-  }
-
 }
