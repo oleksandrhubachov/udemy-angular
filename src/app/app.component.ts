b0de361306f80f801e21782cb4afd89ba3775fec
Bishop-dev <alex.bishop@yandex.ru>
2020-05-13 00:50:33 +0300

    209 - Reactive: Arrays of Form Controls(FormArray)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,21 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
+    console.log(this.signupForm);
+  }
 
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  addHobby() {
+    const formControl = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(formControl);
   }
 }
