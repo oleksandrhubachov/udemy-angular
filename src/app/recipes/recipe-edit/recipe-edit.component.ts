2322d393e421e233ab42de2e34cb67a0e9cc813a
Bishop-dev <alex.bishop@yandex.ru>
2020-05-23 23:02:42 +0300

    230.3 - adding new ingredient controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -60,7 +60,7 @@ export class RecipeEditComponent implements OnInit {
   }
 
   getControls() {
-    return ( <FormArray> this.recipeForm.get('ingredients')).controls;
+    return (<FormArray> this.recipeForm.get('ingredients')).controls;
   }
 
   onAddIngredient() {
