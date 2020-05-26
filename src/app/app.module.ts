8e78ee03a7d8238f0103830423455b5260cbf3d3
Bishop-dev <alex.bishop@yandex.ru>
2020-05-27 00:53:01 +0300

    250 - Module introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {ShortenPipe} from './shorten.pipe';
-import { FilterPipe } from './filter.pipe';
-import { ReversePipe } from './reverse.pipe';
-import { OrderByPipe } from './order-by.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShortenPipe,
-    FilterPipe,
-    ReversePipe,
-    OrderByPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
