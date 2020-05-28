bac2c42d607ba8da99be5e09b86128a5dd9e7c56
Bishop-dev <alex.bishop@yandex.ru>
2020-05-28 22:34:41 +0300

    260 - using a service for http requests

---
+++ src/app/posts.service.ts
@@ -0,0 +1,40 @@
+import {Post} from './post.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  createAndStorePost(postData: Post) {
+    this.http
+      .post<{[key: string]: Post}>(
+        'https://ng-complete-guide-3c763.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http.get<{[key: string]: Post}>('https://ng-complete-guide-3c763.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+
+      });
+  }
+}
