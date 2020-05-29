e33bc6715df1f7a6b861a11562b9875672bf1cdc
Bishop-dev <alex.bishop@yandex.ru>
2020-05-29 23:27:08 +0300

    268 - adding query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Post} from './post.model';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {catchError, map} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
@@ -27,9 +27,14 @@ export class PostsService {
   }
 
   fetchPosts() {
-    return this.http.get<{[key: string]: Post}>('https://ng-complete-guide-3c763.firebaseio.com/posts.json',
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('page', '0');
+    return this.http.get<{[key: string]: Post}>(
+      'https://ng-complete-guide-3c763.firebaseio.com/posts.json',
       {
-          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+          headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         }
       )
       .pipe(map(responseData => {
