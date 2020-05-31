40d8860f146eef7b6125342be96eeff0c2d0bca7
Bishop-dev <alex.bishop@yandex.ru>
2020-05-31 21:15:22 +0300

    277 - module introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,14 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {Post} from './post.model';
-import {PostsService} from './posts.service';
-import {Subscription} from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  loadedPosts = [];
-  isLoading = false;
-  error = null;
-  private errorSubscription: Subscription;
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  constructor(private http: HttpClient, private postsService: PostsService) {}
-
-  ngOnInit() {
-    this.errorSubscription = this.postsService.error.subscribe(errorMessage => {
-      this.error = errorMessage;
-    });
-    this.onFetchPosts();
-  }
-
-  onCreatePost(postData: Post) {
-    this.postsService.createAndStorePost(postData);
-  }
-
-  onFetchPosts() {
-    this.isLoading = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isLoading = false;
-      this.loadedPosts = posts;
-      this.error = null;
-    }, (error) => {
-      this.error = error.error.error;
-      this.isLoading = false;
-    });
-  }
-
-  onClearPosts() {
-    this.postsService.deletePosts().subscribe(() => {
-      this.loadedPosts = [];
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.errorSubscription.unsubscribe();
-  }
-
-  onHandleError() {
-    this.error = null;
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
