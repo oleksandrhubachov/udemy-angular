32ad06b3a5fa5d995b19840d8cad28c7145c7559
Bishop-dev <alex.bishop@yandex.ru>
2020-06-07 22:13:07 +0300

    313 - Preparing Programmatic Creation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
 
 @Component({
   selector: 'app-auth',
@@ -14,7 +15,8 @@ export class AuthComponent {
   error: string = null;
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   onSwitchMode() {
@@ -46,14 +48,20 @@ export class AuthComponent {
       errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     );
-
     authForm.reset();
   }
 
   onHandleError() {
     this.error = null;
   }
+
+  private showErrorAlert(message: string) {
+    const alertComponentComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    // alertComponentComponentFactory.
+  }
+
 }
